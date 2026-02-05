2f775ca7197e2d875600932549e90dfec773b9af
stanleetham-dot <stanleetham@gmail.com>
2026-02-05 13:58:14 +0800

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -1,8 +1,7 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import 'leaflet/dist/leaflet.css';
-import './index.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
